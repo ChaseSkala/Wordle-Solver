dea73e42f520b8d9fdc923f714f6685a8c914e1b
ChaseSkala <Chase@cgcg.biz>
2025-07-03 14:31:58 -0400

    Add files via upload

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-slfuk8sqi0barrt2#%$fyo#zm-hc$&tsqlx)8qv-o0yxqw@(v%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
